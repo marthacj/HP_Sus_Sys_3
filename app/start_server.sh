e8f8977d32b14ff7efb2e88ad3ec8ceed6d59f92
marthacj <121900547+marthacj@users.noreply.github.com>
2024-08-26 15:26:32 +0100

    Update start_server.sh

--- app/start_server.sh
+++ app/start_server.sh
@@ -5,12 +5,12 @@ cd "$(dirname "$0")"
 
 # Echo the current directory and the command being run
 echo "Current directory: $(pwd)"
-echo "Running command: ./ollama serve"
+echo "Running command: Ollama serve"
 
 # Set environment variable for model path
-export OLLAMA_MODELS="$(pwd)/ollama/models"
+export OLLAMA_MODELS="~/.ollama/models"
 echo "Ollama Model path set to: $OLLAMA_MODELS"
 
-chmod +x ./ollama
+chmod +x Ollama 
 # Start the ollama server
-./ollama serve
+Ollama serve
